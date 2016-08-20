44fbfa5476bfd4b6798fff9b7149b9c44e92e3a0
Andrew Ingraham <acingraham@gmail.com>
2016-08-20 16:01:25 +0000

    Add script to updte media.  This is run as a cron job.

---
+++ updateMedia.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PATH=/home/pi/.nvm/versions/node/v4.4.7/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/opt/bin
+
+node /home/pi/AwwFrame/src/reddit.js &
+
