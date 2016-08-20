cb74613393cc46bc600d4aa6522dea95f6d25773
Andrew Ingraham <andrewingraham@Andrews-MacBook-Air-3.local>
2016-08-20 01:52:46 -0400

    Use promises instead of pipes.  Eventually I'd like to return to using pipes, but for now they're causing too many headaches.

--- src/reddit.js
+++ src/reddit.js
@@ -1,12 +1,16 @@
-var uuid      = require('uuid'),
-    path      = require('path'),
-    fs        = require('fs'),
-    request   = require('request'),
-    through2  = require('through2'),
-    db        = require('./db'),
-    Promise   = require('bluebird'),
-    gifyParse = require('gify-parse'),
-    ONE_HOUR  = 3600000;
+var uuid                 = require('uuid'),
+    path                 = require('path'),
+    rp                   = require('request-promise'),
+    fsp                  = require('fs-promise'),
+    through2             = require('through2'),
+    db                   = require('./db'),
+    gifyParse            = require('gify-parse'),
+    ONE_HOUR             = 3600000,
+    SUPPORTED_EXTENSIONS = {
+      '.jpg' : true,
+      '.png' : true,
+      '.gif' : true
+    };
 
 function removeQueryString(url) {
   return url.split('?').shift();
@@ -23,68 +27,63 @@ function getExtension(url) {
 }
 
 function getMedia() {
-  db.removeExcessMedia()
-    .then(function() {
-      request
-        .get('https://www.reddit.com/r/aww.json')
-        .pipe(through2.obj(
-          function(chunk, enc, done) {
-            this.response = this.response || '';
-            this.response += chunk;
-            done();
-          },
-          function(done) {
-            var outStream = this;
-            JSON.parse(this.response).data.children.forEach(function(child) {
-              outStream.push({
-                caption : child.data.title,
-                originalUrl: child.data.url
+  return rp('https://www.reddit.com/r/aww.json')
+    .then(function(res) {
+      var children = JSON.parse(res).data.children;
+      var media = children.reduce(function(agg, child) {
+        var url = removeQueryString(child.data.url);
+        url = convertGifvToGif(url);
+
+        var ext = getExtension(url);
+
+        if (SUPPORTED_EXTENSIONS[ext]) {
+          agg.push({
+            caption     : child.data.title,
+            filename    : uuid.v4() + ext,
+            extension   : ext,
+            originalUrl : child.data.url,
+            url         : url
+          });
+        }
+
+        return agg;
+      }, []);
+
+      media.forEach(function(chunk) {
+        db.exists(chunk.originalUrl)
+        .then(function(exists) {
+          console.log('exists: ' + exists);
+          if (exists) {
+            return db.updateTimestamp(chunk.originalUrl)
+              .then(function() {
+                console.log('Updated ' + chunk.url)
               });
-            });
-            done();
+          } else {
+            var filepath = __dirname + '/public/img/' + chunk.filename;
+            return rp(chunk.url, { encoding : null })
+              .then(function(res) {
+                return fsp.writeFile(filepath, res);
+              })
+              .then(function() {
+                if (chunk.extension === '.gif') {
+                  return fsp.readFile(filepath)
+                    .then(function(buffer) {
+                      chunk.duration = gifyParse.getInfo(buffer).duration;
+                    });
+                }
+              })
+              .then(function() {
+                return db.addMedia(chunk);
+              })
+              .then(function() {
+                console.log('Added ' + chunk.url)
+              });
           }
-        ))
-        .pipe(through2.obj(function(chunk, enc, done) {
-          var url = removeQueryString(chunk.originalUrl);
-          url = convertGifvToGif(url);
-          var ext = getExtension(url);
-
-          chunk.extension = ext;
-
-          if (ext === '.jpg' || ext === '.png' || ext === '.gif') {
-            chunk.url = url;
-            chunk.filename = uuid.v4() + ext;
-            console.log('Adding ' + chunk.url);
-            this.push(chunk);
-          }
-
-          done();
-        }))
-        .pipe(through2.obj(function(chunk, enc, done) {
-          db.exists(chunk.originalUrl)
-            .then(function(exists) {
-              console.log('exists: ' + exists);
-              if (exists) {
-                db.updateTimestamp(chunk.originalUrl);
-              } else {
-                var filepath = __dirname + '/public/img/' + chunk.filename;
-                var writeStream = fs.createWriteStream(filepath);
-                writeStream.on('finish', function() {
-                  if (chunk.extension === '.gif') {
-                    // TODO - Make async
-                    var buffer = fs.readFileSync(filepath);
-                    chunk.duration = gifyParse.getInfo(buffer).duration;
-                  }
-
-                  db.addMedia(chunk);
-                });
-
-                request.get(chunk.url)
-                  .pipe(writeStream);
-              }
-              done();
-            });
-        }));
+        })
+        .catch(function(err) {
+          console.error('An error occurred with ' + chunk.originalUrl + ': ' + err);
+        });
+      });
     });
 }
 
