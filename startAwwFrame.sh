0c7e08e1f087eead23dfc281cf8c0253c6375daf
Andrew Ingraham <acingraham@gmail.com>
2016-08-20 16:05:40 +0000

    Stop startAwwFrame script from running reddit.js

--- startAwwFrame.sh
+++ startAwwFrame.sh
@@ -2,5 +2,4 @@
 PATH=/home/pi/.nvm/versions/node/v4.4.7/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/opt/bin
 
 nodemon /home/pi/AwwFrame/src/server.js &
-nodemon /home/pi/AwwFrame/src/reddit.js &
 
